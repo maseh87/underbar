0aab2e1e9da2672c5a49d8eba153261c5462cb52
joshWyatt <joshuawyatt123@gmail.com>
2014-05-07 15:26:02 -0700

    Implemented 'delay' function.

--- src/underbar.js
+++ src/underbar.js
@@ -300,6 +300,10 @@ var _ = {};
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var storedArgs = Array.prototype.slice.call(arguments, 2);
+    setTimeout(function() {
+      func.apply(this, storedArgs);
+    }, wait);
   };
 
 
