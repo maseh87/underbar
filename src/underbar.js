57f4d54f40d71d5057e80c08ff1af89b68279d6f
joshWyatt <joshuawyatt123@gmail.com>
2014-05-08 11:56:52 -0700

    Implemented 'difference' function.

--- src/underbar.js
+++ src/underbar.js
@@ -396,6 +396,14 @@ var _ = {};
   // Take the difference between one array and a number of other arrays.
   // Only the elements present in just the first array will remain.
   _.difference = function(array) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return _.reduce(args, function(results, array) {
+      return _.filter(results, function(resultsItem) {
+        return !(_.some(array, function(arrayItem) {
+          return (resultsItem === arrayItem);
+        }));
+      });
+    }, arguments[0]);
   };
 
 
